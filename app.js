aeebcce458f3b790a6247d76a949a6da7545fe57
iucu0 <igna7_17@hotmail.com>
2023-06-21 22:25:09 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/contact', (req, res) => {
+    res.status(200).sendFile(__dirname + '/public/contact.html');
+})
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+})
